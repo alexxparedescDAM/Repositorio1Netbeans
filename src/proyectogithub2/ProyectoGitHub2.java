ef147e28abcebb9b773a05861b92741378dfa2de
admin <admin@DESKTOP-1M53M97.ciclo.local>
2018-03-01 09:58:26 +0100

    Subida del proyecto GitHub al repositoria local Netbeans. 01-03-2018

---
+++ src/proyectogithub2/ProyectoGitHub2.java
@@ -0,0 +1,17 @@
+
+package proyectogithub2;
+
+/**
+ *
+ * @author admin
+ */
+public class ProyectoGitHub2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+    
+}
